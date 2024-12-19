e5716aa50a04003fb92ba32f2efe5a0a28556edf
Казыбек Аскарбек <qazybi@gmail.com>
2024-12-19 21:09:37 +0300

    update of the arguments

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,23 +1,11 @@
-# catboost
 export ENABLE_CLEARML=1
-# export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 python scripts/train.py \
     --train_path data/raw/train.csv \
-    --task_name catboost_rubert_features_title_longer_sep_embed_stratified_kfold3 \
+    --task_name clip_embed \
     --model_name CatBoost \
-    --use_stratified_kfold True \
-    --embed_add_as_separate_columns True
-
-
-# ridge
-# python scripts/train.py \
-#     --train_path data/raw/train.csv \
-#     --task_name ridge_baseline \
-#     --model_name Ridge
-
-# lightgbm
-# python scripts/train.py \
-#     --train_path data/raw/train.csv \
-#     --task_name lightgbm_baseline \
-#     --model_name LightGBM
+    --use_stratified_kfold False \
+    --embed_add_as_separate_columns True \
+    --add_image_features True \
+    --use_prep_data_cache False \
+    --use_truncated_embeddings True
